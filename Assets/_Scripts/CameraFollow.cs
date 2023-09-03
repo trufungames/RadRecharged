c5f456f3fde9558fd9e7efd0c31621081e9abdb8
trufun101 <chros.bryant@gmail.com>
2023-09-02 22:14:17 -0500

    Basic Player and Camera movement

--- Assets/_Scripts/CameraFollow.cs
+++ Assets/_Scripts/CameraFollow.cs
@@ -21,8 +21,8 @@ public class CameraFollow : MonoBehaviour
             targetPos.y += offsetPosition.y;
             targetPos.z = transform.position.z;
 
-            //targetPos.x = Mathf.Clamp(targetPos.x, boundsMin.x, boundsMax.x);
-            //targetPos.y = Mathf.Clamp(targetPos.y, boundsMin.y, boundsMax.y);
+            targetPos.x = Mathf.Clamp(targetPos.x, boundsMin.x, boundsMax.x);
+            targetPos.y = Mathf.Clamp(targetPos.y, boundsMin.y, boundsMax.y);
 
             float t = 1f - Mathf.Pow(1f - timeOffset, Time.deltaTime * 30);
             transform.position = Vector3.Lerp(startPos, targetPos, t);
